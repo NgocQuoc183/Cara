59a4b70702f2f5c6ac99fd0c0fce247f9fa14a1c
NgocQuoc183 <phamngocquoc921803@gmail.com>
2024-10-16 21:47:38 +0700

    Cara

---
+++ src/java/quoc/dev/data/dao/Database.java
@@ -0,0 +1,38 @@
+package quoc.dev.data.dao;
+
+import quoc.dev.data.impl.CategoryImpl;
+import quoc.dev.data.impl.OrderImpl;
+import quoc.dev.data.impl.OrderItemImpl;
+import quoc.dev.data.impl.ProductImpl;
+import quoc.dev.data.impl.UserImpl;
+
+public class Database extends DatabaseDao {
+
+    
+    @Override
+    public CategoryDao getCategoryDao() {
+        // TODO Auto-generated method stub
+        return new CategoryImpl();
+    }
+    @Override
+    public OrderItemDao getOrderItemDao() {
+        // TODO Auto-generated method stub
+        return new OrderItemImpl();
+    }
+    @Override
+    public OrderDao getOrderDao() {
+        // TODO Auto-generated method stub
+        return new OrderImpl();
+    }
+    @Override
+    public UserDao getUserDao() {
+        // TODO Auto-generated method stub
+        return new UserImpl();
+    }
+	@Override
+	public ProductDao getProductDao() {
+		// TODO Auto-generated method stub
+		return new ProductImpl();
+	}
+
+}
